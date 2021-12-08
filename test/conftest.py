db042d203c5c9d0d67bf285e32dcc2ae978c0f1e
gsy0911 <yoshiki0911@gmail.com>
2021-12-08 23:58:40 +0900

    format and update setup.py

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,6 @@ import sys
 # テスト対象のファイルへのパスを通している
 # pytestの設定
 PARENT_PATH = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
-SOURCE_PATH = PARENT_PATH.rsplit('/', 1)[0]
+SOURCE_PATH = PARENT_PATH.rsplit("/", 1)[0]
 
 sys.path.append(f"{SOURCE_PATH}")
